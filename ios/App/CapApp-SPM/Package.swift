3d40e40b00784a68e17edc9f9c1cd6677ed46c64
lunacalmapp <winsenwang703@gmail.com>
2026-08-01 08:46:53 +0700

    Add files via upload

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -2,7 +2,6 @@
 import PackageDescription
 
 // DO NOT MODIFY THIS FILE - managed by Capacitor CLI commands
-// （FirebaseMessaging 这一条是手动加的额外原生依赖，不是 Capacitor 插件，Capacitor CLI 不会碰它）
 let package = Package(
     name: "CapApp-SPM",
     platforms: [.iOS(.v15)],
@@ -12,16 +11,14 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.4.1"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.4.1")
     ],
     targets: [
         .target(
             name: "CapApp-SPM",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm"),
-                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk")
+                .product(name: "Cordova", package: "capacitor-swift-pm")
             ]
         )
     ]
